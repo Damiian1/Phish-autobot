40d74baf5c7f03009402b9d4d1297aabb8e9cba8
Damiian1 <34922879+Damiian1@users.noreply.github.com>
2019-04-14 09:50:26 +0800

    Update phish-autobot.py

--- phish-autobot.py
+++ phish-autobot.py
@@ -4,15 +4,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+import re  
 import random
 import string
 import time
 import os
 
-website_link="https://decoy-captive.000webhostapp.com/real/portal.html"
+website_link="https://decoy-captive.000webhostapp.com/portal.html"
 def randomstring(stringLength=10):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
+
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+
 
 username = randomstring()
 
@@ -28,7 +37,7 @@ element_for_submit="login"
 
 
 
-browser = webdriver.Safari() 
+browser = webdriver.Chrome(chrome_options=options)
 browser.get((website_link))    
 
 browser.find_element_by_id("auth_email").send_keys(email)
@@ -36,9 +45,10 @@ browser.find_element_by_id("auth_user").send_keys(username)
 browser.find_element_by_id("auth_pass").send_keys(password)
 browser.find_element_by_id("checkbox").click()
 browser.find_element_by_id("login").click()
+browser.save_screenshot('screen.png')
 
 
-delay = 10 # seconds
+delay = 5 # seconds
 try:
     myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, 'error')))
     print("Legitimate captive portal!")
